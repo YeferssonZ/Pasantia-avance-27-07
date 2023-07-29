16278959c28948215d6c9ada45f8d4fb606578a5
unknown <yefersson.zuniga.tecsup.edu.pe>
2023-07-29 18:02:41 -0500

    Avance 29/07/2023

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    stock = models.DecimalField(max_digits=10, decimal_places=2)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    imagen = models.ImageField(upload_to='productos/', null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Tutorial(models.Model):
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    contenido = models.TextField()
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
+
+class Comentario(models.Model):
+    texto = models.TextField()
+    fecha_hora = models.DateTimeField(auto_now_add=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.usuario.username} - {self.producto.nombre}'
+
+class Carrito(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    productos = models.ManyToManyField(Producto)
+    creado = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Carrito de {self.usuario.username}'
+
+class MetodoPago(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
--- tienda/settings.py
+++ tienda/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4eov1t#g1gabhlhbi+hw5&h$+fvv4s#1)x9)m&7k%zl-arvjh8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
